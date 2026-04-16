949af923a44992405590fe2ddca1f079875f3c75
alcideskapunda <alcideskapunda22@gmail.com>
2026-04-15 22:20:46 -0300

    fix(database): enable sensitive data logging and detailed errors for development

--- src/CapitalFlow.Persistence/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/CapitalFlow.Persistence/DependencyInjection/DependencyInjectionExtensions.cs
@@ -38,6 +38,10 @@ public static class DependencyInjectionExtensions
                         mysqlOptions
                             .EnableRetryOnFailure()
                             .TranslateParameterizedCollectionsToConstants());
+
+                // DEV ONLY
+                options.EnableSensitiveDataLogging();
+                options.EnableDetailedErrors();
             },
             contextLifetime: ServiceLifetime.Scoped,
             optionsLifetime: ServiceLifetime.Singleton);
